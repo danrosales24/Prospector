0435ed393e1c28c4ca932897931d502b0fc86b6e
danrosales24 <52152236+danrosales24@users.noreply.github.com>
2019-11-22 15:00:44 -0500

    added to Prospector and made CardProspector

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -9,27 +9,54 @@ public class Prospector : MonoBehaviour {
 
 	static public Prospector 	S;
 
-	[Header("Set in Inspector")]
-	public TextAsset			deckXML;
+    [Header("Set in Inspector")]
+    public TextAsset deckXML;
+    public TextAsset layoutXML;
+    public float xOffset = 3;
+    public float yOffset = -2.5f;
+    public Vector3 layoutCenter;
+    public Vector2 fsPosMid = new Vector2(.5f, .90f);
+    public Vector2 fsPosRun = new Vector2(.5f, .75f);
+    public Vector2 fsPosMid2 = new Vector2(.4f, 1.0f);
+    public Vector2 fsPosEnd = new Vector2(.5f, .95f);
+    public float reloadDelay = 2f;
+    public Text gameOverText, roundResultText, highScoreText;
 
+    [Header("Set Dynamically")]
+    public Deck deck;
+    public Layout layout;
+    public List<CardProspector> drawPile;
+    public Transform layoutAnchor;
+    public CardProspector target;
+    public List<CardProspector> tableau;
+    public List<CardProspector> discardPile;
 
-	[Header("Set Dynamically")]
-	public Deck					deck;
-
-	void Awake(){
+    void Awake(){
 		S = this;
 	}
 
 	void Start() {
 		deck = GetComponent<Deck> ();
 		deck.InitDeck (deckXML.text);
-        Deck.Shuffle(ref deck.cards); // This shuffles the deck by reference // a
-        Card c;
-        for (int cNum = 0; cNum < deck.cards.Count; cNum++)
-        {
-            c = deck.cards[cNum];
-            c.transform.localPosition = new Vector3((cNum % 13) * 3, cNum /
-        }
+        Deck.Shuffle(ref deck.cards);
+
+        layout = GetComponent<Layout>();
+        layout.ReadLayout(layoutXML.text);
+        drawPile = ConvertListCardsToListCardProspectors(deck.cards);
+     
+
     }
+    List<CardProspector> ConvertListCardsToListCardProspectors(List<Card> lCD)
+    {
+        List<CardProspector> lCP = new List<CardProspector>();
+        CardProspector tCP;
+        foreach (Card tCD in lCD)
+        {
+            tCP = tCD as CardProspector;
+            lCP.Add(tCP);
+        }
+        return (lCP);
+    }
+
 
 }
